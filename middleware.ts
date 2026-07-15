22255da1f1b837bc3502961d7086a9a3c3d3b725
Venkat Koushik <koushikprivate35@gmail.com>
2026-07-15 10:00:24 +0530

    Add Clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextRequest } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
-    "/dashboard(.*)",
-    "/tasks(.*)",
+  '/dashboard(.*)',
+  '/tasks(.*)',
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) {
-        await auth.protect();
-    }
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)', 
+    '/', 
+    '/(api|trpc)(.*)',
+  ],
 };
-
-function isPublicRoute(req: NextRequest): boolean {
-    return !isProtectedRoute(req);
-}
-
